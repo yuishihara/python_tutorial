496539012e80ad4e5882aa0f3dc535dc067d4264
Yu Ishihara <yuishihara1225@gmail.com>
2019-03-09 14:00:18 +0900

    Add section 4.5

---
+++ 4.more_control_flow_tools/pass_statements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+def initlog(*args):
+    pass  # Remember to implement this!
+
+
+class MyEmptyClass:
+    pass
+
+if __name__ == "__main__":
+    # This will cause a busy wait
+    # while True:
+    #    pass
+    pass
