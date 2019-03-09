01e91611309dfacbbbd86713b5c17e29ef138dce
Yu Ishihara <yuishihara1225@gmail.com>
2019-03-09 14:11:19 +0900

    Add section 4.6

---
+++ 4.more_control_flow_tools/defining_functions.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+
+def fib(n):  # write Fibonacci series up to n
+    """This first line is a docstring"""
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+
+def fib2(n):
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a + b
+    return result
+
+
+if __name__ == "__main__":
+    fib(2000)
+
+    f = fib
+    f(100)
+
+    # functions return None in case no return statement exist
+    print(fib(0))
+
+    f100 = fib2(100)
+    print(f100)
