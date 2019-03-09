5b469e2a7cde6f518c2a04deef564d4b2879c829
Yu Ishihara <yuishihara1225@gmail.com>
2019-03-09 13:11:45 +0900

    Add section 4.2

---
+++ 4.more_control_flow_tools/for_statements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+if __name__ == "__main__":
+    words = ['cat', 'window', 'defenestrate']
+    for w in words:
+        print(w, len(w))
+
+    # Don't do below to avoid infinite loop
+    # for w in words:
+    # instead do
+    for w in words[:]:
+        if len(w) > 6:
+            words.insert(0, w)
+    print(words)
