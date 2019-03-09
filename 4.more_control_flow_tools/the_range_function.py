208a82d5de76cc9a04bb2a004569957b558bd6a3
Yu Ishihara <yuishihara1225@gmail.com>
2019-03-09 13:21:37 +0900

    Add section 4.3

---
+++ 4.more_control_flow_tools/the_range_function.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+if __name__ == "__main__":
+    for i in range(5):
+        print(i)
+    for i in range(5, 10):
+        print(i, end=',')
+    print()
+    for i in range(0, 10, 3):
+        print(i, end=',')
+    print()
+    for i in range(-10, -100, -30):
+        print(i, end=',')
+    print()
+
+    # consider using enumerate()
+    a = ['Mary', 'had', 'a', 'little', 'lamb']
+    for i in range(len(a)):
+        print(i, a[i])
+
+    # range does not return a list, returns a iterable instead.
+    print(range(10))
+
+    # list creates a list from iterable
+    print(list(range(5)))
+     
