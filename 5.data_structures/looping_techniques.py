2001ca492708492cd3f9892ad3255c09b878d94f
Yu Ishihara <yu_ishihara.a2@keio.jp>
2019-03-09 20:40:39 +0900

    Add section 5.6

---
+++ 5.data_structures/looping_techniques.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    # Looping through dictionaries
+    knights = {'gallhad': 'the pure', 'robin': 'the brave'}
+    for k, v in knights.items():
+        print(k, v)
+
+    # Looping through sequences with the position index
+    for i, v in enumerate(['tic', 'tac', 'toe']):
+        print(i, v)
+
+    questions = ['name', 'quest', 'favorite color']
+    answers = ['lancelot', 'the holy grail', 'blue']
+
+    for q, a in zip(questions, answers):
+        print('What is your {0}? It is {1}.'.format(q, a))
+
+    # Revesed order loop
+    for i in reversed(range(1, 10, 2)):
+        print(i)
+
+    basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+    for f in sorted(set(basket)):
+        print(f)
+
+    import math
+    raw_data = [56.2, float("NaN"), 51.7, 55.3, 52.5, float("NaN"), 47.8]
+    filtered_data = []
+    for value in raw_data:
+        if not math.isnan(value):
+            filtered_data.append(value)
+
+    print(filtered_data)
