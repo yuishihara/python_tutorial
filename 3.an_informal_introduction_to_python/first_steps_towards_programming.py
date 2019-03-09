3c08c0eb64de461ba69d7422867920947745109b
Yu Ishihara <yuishihara1225@gmail.com>
2019-03-09 12:46:36 +0900

    Add section 3.2

---
+++ 3.an_informal_introduction_to_python/first_steps_towards_programming.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+if __name__ == "__main__":
+    a, b = 0, 1
+    while a < 10:
+        print(a)
+        a, b = b, a+b
+
+    i = 256 * 256
+    print('The value of i is', i)
+
+    a, b = 0, 1
+    while a < 1000:
+        print(a, end=',')
+        a, b = b, a+b
