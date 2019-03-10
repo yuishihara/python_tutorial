daca930bc6b4e2722c83c42a367b65897e247ced
Yu Ishihara <yuishihara1225@gmail.com>
2019-03-10 09:58:26 +0900

    Add section 6.3

---
+++ 6.modules/the_dir_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import fibo, sys
+
+
+if __name__ == "__main__":
+    print(dir(fibo))
+    print(dir(sys))
+    
+    a = [1, 2, 3, 4, 5]
+    print(dir())
+
+    import builtins
+    print(dir(builtins))
