fdeead47c1f4f134744f9d14603ca0eb58e98012
Yu Ishihara <yuishihara1225@gmail.com>
2019-03-10 16:36:46 +0900

    Add section 9.2

---
+++ 9.classes/python_scopes_and_namespaces.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+def scope_test():
+    def do_local():
+        spam = "local spam"
+
+    def do_nonlocal():
+        nonlocal spam
+        spam = "nonlocal spam"
+
+    def do_global():
+        global spam
+        spam = "global spam"
+
+    spam = "test spam"
+    do_local()
+    print("After local assignment:", spam)
+    do_nonlocal()
+    print("After nonlocal assignment:", spam)
+    do_global()
+    print("After global assignment:", spam)
+
+
+def scopes_and_namespaces_example():
+    scope_test()
+    print("In global scope:", spam)
+
+
+if __name__ == "__main__":
+    scopes_and_namespaces_example()
